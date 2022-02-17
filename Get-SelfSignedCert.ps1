6adeb7d536f316aef5d70543e5700113424f8707
Jeremy Brown <42386913+BananaJama@users.noreply.github.com>
2022-02-16 22:09:51 -0500

    Adding Graph Connect

--- Get-SelfSignedCert.ps1
+++ Get-SelfSignedCert.ps1
@@ -3,4 +3,11 @@ $Splat = @{
     Password = (ConvertTo-SecureString -AsPlainText -Force -String 'SecurePassword')
 }
 $Cert = Get-PfxCertificate @Splat
-$Cert | Format-List
+# $Cert | Format-List
+
+$Splat = @{
+    ClientId = '6300e930-22a1-4313-aa71-3a1e62fce15d'
+    TenantId = 'fb24bda8-d77a-4f50-8f02-d6464359cc27'
+    Certificate = $Cert
+}
+Connect-Graph @Splat
